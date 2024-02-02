2b5d510cb5f7cb70cc4a483d90101ff046782406
P4lm4D3v <matejadjokic23@gmail.com>
2024-02-02 16:58:43 +0100

    init

---
+++ lib/nice_colors.dart
@@ -0,0 +1,42 @@
+library nice_colors;
+
+import 'dart:ui';
+
+class NiceColors {
+  //? BLACK
+  static get fullBlack => const Color(0x00000000);
+  static get oilBlack => const Color(0xff0c0c0c);
+  static get obsidian => const Color(0xff0b1215);
+  static get richBlack => const Color(0xff020d19);
+
+  //? GRAY
+  static get darkSlateGray => const Color(0xff0d1717);
+  static get charcoalGray => const Color(0xff171717);
+
+  //? WHITE
+  static get fullWhite => const Color(0xffffffff);
+
+  //? RED
+
+  //? ORANGE
+  static get creame => const Color(0xfffef5ea);
+
+  //? YELLOW
+
+  //? GREEN
+  static get mojitoBreeze => const Color(0xffffedfa);
+
+  //? BLUE
+  static get midnightBlue => const Color(0xff101720);
+  static get deepNavyBlue => const Color(0xff011222);
+  static get cloudWisp => const Color(0xffe7f6ff);
+  static get coldSteel => const Color(0xffe3edf8);
+
+  //? INDIGO
+
+  //? VIOLET
+  static get lavenderWhip => const Color(0xfffaf5ff);
+
+  //? PINK
+  static get candyFloss => const Color(0xffffedfa);
+}
